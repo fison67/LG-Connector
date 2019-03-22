a8f7889a0d29a0186d392e42d9a9b012ba0c6bfa
chals <fison67@nate.com>
2019-03-22 13:16:51 +0900

    Update lg-dryer.groovy

--- devicetypes/fison67/lg-dryer.src/lg-dryer.groovy
+++ devicetypes/fison67/lg-dryer.src/lg-dryer.groovy
@@ -54,7 +54,40 @@ PROCESS_STATE_VALUE = [
     5: [val: "@WM_STATE_COOLING_W", str: ["EN":"COOLING", "KR":"쿨링"] ],
     6: [val: "@WM_STATE_ANTI_CREASE_W", str: ["EN":"ANTI CREASE", "KR":"구김방지 중"] ],
     7: [val: "@WM_STATE_END_W", str: ["EN":"END", "KR":"종료 상태"] ]
+]
 
+@Field
+COURSE_VALUE = [
+	1: [val: "@REFRESH", str: ["EN":"REFRESH", "KR":"REFRESH"] ],
+	2: [val: "@WM_DRY24_COURSE_COTTON_SOFT_W", str: ["EN":"COTTON SOFT", "KR":"타월"] ],
+	4: [val: "@WM_DRY24_COURSE_BULKY_ITEM_W", str: ["EN":"BULKY", "KR":"이불"] ],
+	5: [val: "@WM_DRY24_COURSE_EASY_CARE_W", str: ["EN":"EASY_CARE", "KR":"셔츠"] ],
+	7: [val: "@WM_DRY24_COURSE_COTTON_W", str: ["EN":"COURSE COTTON", "KR":"표준"] ],
+	8: [val: "@WM_DRY24_COURSE_SPORTS_WEAR_W", str: ["EN":"SPORTS WEAR", "KR":"기능성의류"] ],
+	9: [val: "@WM_DRY24_COURSE_QUICK_DRY_W", str: ["EN":"QUICK DRY", "KR":"소량급속"] ],
+	11: [val: "@WM_DRY24_COURSE_WOOL_W", str: ["EN":"WOOL", "KR":"울/섬세"] ],
+	12: [val: "@WM_DRY24_COURSE_RACK_DRY_W", str: ["EN":"RACK DRY", "KR":"선반건조"] ],
+	13: [val: "@WM_DRY24_COURSE_COOL_AIR_W", str: ["EN":"COOL AIR", "KR":"송풍"] ],
+	14: [val: "@WM_DRY24_COURSE_WARM_AIR_W", str: ["EN":"WARM AIR", "KR":"온풍"] ],
+    15: [val: "@WM_DRY24_COURSE_BEDDING_BRUSH_W", str: ["EN":"BEDDING BRUSH", "KR":"침구털기"] ],
+    16: [val: "@WM_DRY24_COURSE_STERILIZATION_W", str: ["EN":"STERILIZATION", "KR":"살균"] ],
+    17: [val: "@WM_DRY24_COURSE_POWER_W", str: ["EN":"STERILIZATION", "KR":"강력"] ],
+    102: [val: "@WM_WW_DRYER_SMARTCOURSE_GYM_CLOTHES_W", str: ["EN":"GYM CLOTHES", "KR":"운동복"] ],
+    103: [val: "@WM_WW_DRYER_SMARTCOURSE_BLANKET_W", str: ["EN":"BLANKET", "KR":"이불"] ],
+    104: [val: "@WM_WW_DRYER_SMARTCOURSE_BLANKET_REFRESH_W", str: ["EN":"BLANKET REFRESH", "KR":"이불 REFRESH"] ],
+    105: [val: "@WM_WW_DRYER_SMARTCOURSE_RAINY_SEASON_W", str: ["EN":"RAINY SEASON", "KR":"장마철 세탁"] ],
+    106: [val: "@WM_WW_DRYER_SMARTCOURSE_SINGLE_GARMENTS_W", str: ["EN":"SINGLE GARMENTS", "KR":"한벌 세탁"] ],
+    107: [val: "@WM_WW_DRYER_SMARTCOURSE_DEODORIZATION_W", str: ["EN":"DEODORIZATION", "KR":"냄새 제거"] ],
+    108: [val: "@WM_WW_DRYER_SMARTCOURSE_SMALL_LOAD_W", str: ["EN":"SMALL LOAD", "KR":"소량 건조"] ],
+    109: [val: "@WM_WW_DRYER_SMARTCOURSE_LINGERIE_W", str: ["EN":"LINGERIE", "KR":"란제리"] ],
+    110: [val: "@WM_WW_DRYER_SMARTCOURSE_EASY_IRON_W", str: ["EN":"LINGERIE", "KR":"촉촉 건조"] ],
+    111: [val: "@WM_WW_DRYER_SMARTCOURSE_SUPER_DRY_W", str: ["EN":"SUPER DRY", "KR":"강력 건조"] ],
+    112: [val: "@WM_WW_DRYER_SMARTCOURSE_ECONOMIC_DRY_W", str: ["EN":"ECONOMIC DRY", "KR":"절약 건조"] ],
+    113: [val: "@WM_WW_DRYER_SMARTCOURSE_BIG_SIZE_ITEM_W", str: ["EN":"BIG SIZE ITEM", "KR":"큰 빨래 건조"] ],
+    114: [val: "@WM_WW_DRYER_SMARTCOURSE_MINIMIZE_WRINKLES_W", str: ["EN":"MINIMIZE WRINKLES", "KR":"구김 완화 건조"] ],
+    115: [val: "@WM_WW_DRYER_SMARTCOURSE_SHOES_FABRIC_DOLL_W", str: ["EN":"SHOES FABRIC DOLL", "KR":"신발/인형 건조"] ],
+    116: [val: "@WM_WW_DRYER_SMARTCOURSE_FULL_SIZE_LOAD_W", str: ["EN":"FULL SIZE LOAD", "KR":"다량건조"] ],
+    117: [val: "@WM_KR_DR_SMARTCOURSE_DENIM_W", str: ["EN":"DENIM", "KR":"청바지"] ]
 ]
 
 metadata {
@@ -67,6 +100,7 @@ metadata {
         attribute "leftMinute", "number"
         attribute "curState", "string"
         attribute "processState", "string"
+        attribute "course", "string"
 	}
 
 	simulator {
@@ -88,17 +122,24 @@ metadata {
     			attributeState("default", label:'Updated: ${currentValue}')
             }
 		}
-        
+        /*
         valueTile("curState_label", "", decoration: "flat", width: 3, height: 1) {
             state "default", label:'State'
         }
         valueTile("curState", "device.curState", decoration: "flat", width: 3, height: 1) {
             state "default", label:'${currentValue}'
         }
+        */
         valueTile("processState_label", "", decoration: "flat", width: 3, height: 1) {
             state "default", label:'Process State'
         }
         valueTile("processState", "device.processState", decoration: "flat", width: 3, height: 1) {
+            state "default", label:'${currentValue}'
+        }
+        valueTile("course_label", "", decoration: "flat", width: 3, height: 1) {
+            state "default", label:'Course'
+        }
+        valueTile("course", "device.course", decoration: "flat", width: 3, height: 1) {
             state "default", label:'${currentValue}'
         }
         valueTile("leftTime_label", "", decoration: "flat", width: 3, height: 1) {
@@ -148,6 +189,15 @@ def setStatus(data){
     
     if(jsonObj.ProcessState != null){
     	sendEvent(name:"processState", value: PROCESS_STATE_VALUE[jsonObj.ProcessState.value as int]["str"][language])
+    }
+    
+    if(jsonObj.Course != null){
+    	def name = "unknown"
+    	def item = COURSE_VALUE[jsonObj.Course.value as int]
+        if(item){
+        	name = COURSE_VALUE[jsonObj.Course.value as int]["str"][language]
+        }
+    	sendEvent(name:"course", value: name)
     }
     
     if(jsonObj.Remain_Time_H != null){
