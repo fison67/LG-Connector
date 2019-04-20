cf7b86f4440aa2d62b776d7ddc01410800e7a611
chals <fison67@nate.com>
2019-04-20 10:16:28 +0900

    Update lg-connector.groovy

--- smartapps/fison67/lg-connector.src/lg-connector.groovy
+++ smartapps/fison67/lg-connector.src/lg-connector.groovy
@@ -62,7 +62,7 @@ def mainPage() {
         
        	section() {
             paragraph "View this SmartApp's configuration to use it in other places."
-            href url:"${apiServerUrl("/api/smartapps/installations/${app.id}/config?access_token=${state.accessToken}")}", style:"embedded", required:false, title:"Config", description:"Tap, select, copy, then click \"Done\""
+            href url:"${localApiServerUrl("${app.id}/config?access_token=${state.accessToken}")}", style:"embedded", required:false, title:"Config", description:"Tap, select, copy, then click \"Done\""
        	}
     }
 }
@@ -142,13 +142,13 @@ def initialize() {
             "Content-Type": "application/json"
         ],
         "body":[
-            "app_url":"${apiServerUrl}/api/smartapps/installations/",
+            "app_url":localApiServerUrl(""),
             "app_id":app.id,
             "access_token":state.accessToken
         ]
     ]
     
-    def myhubAction = new physicalgraph.device.HubAction(options, null, [callback: null])
+    def myhubAction = new hubitat.device.HubAction(options, null, [callback: null])
     sendHubCommand(myhubAction)
     log.debug "server..."
     
@@ -156,7 +156,7 @@ def initialize() {
     updateExternalNetwork()
 }
 
-def dataCallback(physicalgraph.device.HubResponse hubResponse) {
+def dataCallback(hubitat.device.HubResponse hubResponse) {
     def msg, json, status
     try {
         msg = parseLanMessage(hubResponse.description)
@@ -177,7 +177,7 @@ def getDataList(){
             "Content-Type": "application/json"
         ]
     ]
-    def myhubAction = new physicalgraph.device.HubAction(options, null, [callback: dataCallback])
+    def myhubAction = new hubitat.device.HubAction(options, null, [callback: dataCallback])
     sendHubCommand(myhubAction)
 }
 
@@ -202,8 +202,6 @@ def addDevice(){
         	dth = "LG Air Conditioner"
         }else if(type == "dryer"){
         	dth = "LG Dryer"
-        }else if(type == "air_purifier"){
-        	dth = "LG Air PuriFier"
         }
         
         def name = dth;
@@ -263,7 +261,7 @@ def renderConfig() {
             [
                 platform: "SmartThings LG Connector",
                 name: "LG Connector",
-                app_url: apiServerUrl("/api/smartapps/installations/"),
+                app_url: localApiServerUrl(""),
                 app_id: app.id,
                 access_token:  state.accessToken
             ]
@@ -275,15 +273,10 @@ def renderConfig() {
 }
 
 mappings {
-    if (!params.access_token || (params.access_token && params.access_token != state.accessToken)) {
-        path("/config")                         { action: [GET: "authError"] }
-        path("/list")                         	{ action: [GET: "authError"]  }
-        path("/update")                         { action: [POST: "authError"]  }
-        path("/add")                         	{ action: [POST: "authError"]  }
-    } else {
+
         path("/config")                         { action: [GET: "renderConfig"]  }
         path("/list")                         	{ action: [GET: "getDeviceList"]  }
         path("/update")                         { action: [POST: "updateDevice"]  }
         path("/add")                         	{ action: [POST: "addDevice"]  }
-    }
+  
 }
